ca4fb2b2b4e183d8c76d4d012224c614565f5316
TavernCode <levink722@gmail.com>
2023-08-29 15:47:15 +0700

    Update

--- lib/session.ts
+++ lib/session.ts
@@ -4,7 +4,8 @@ import { AdapterUser } from "next-auth/adapters";
 import GoogleProvider from 'next-auth/providers/google';
 import jsonwebtoken from 'jsonwebtoken'
 import { JWT } from 'next-auth/jwt'
-import { SessionInterface } from "@/common.type";
+import { SessionInterface, UserProfile } from "@/common.type";
+import { createUser, getUser } from "./actions";
 
 export const authOptions: NextAuthOptions = {
    providers: [
@@ -27,10 +28,33 @@ export const authOptions: NextAuthOptions = {
    },
    callbacks: {
       async session({ session }) {
-         return session;
+         const email = session?.user?.email as string;
+         try {
+            const data=await getUser(email) as {user?:UserProfile}
+
+            const newSession={
+               ...session,
+               user:{
+                  ...session.user,
+                  ...data?.user
+               }
+            }
+            return newSession;
+         } catch (error){
+            console.log('Error retreiving data user', error);
+            return session
+         }
       },
       async signIn({ user }: { user: AdapterUser | User }) {
          try {
+            const userExists = await getUser(user?.email as string) as {user?:UserProfile}
+
+            if (!userExists.user){
+               await createUser(
+                  user.name as string, 
+                  user.email as string, 
+                  user.image as string);
+            }
 
             return true
          } catch (error: any) {
