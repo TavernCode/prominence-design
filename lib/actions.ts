ad291e2cdfa5b0a01bab783847ce3ce046e5b135
TavernCode <levink722@gmail.com>
2023-08-29 10:29:41 +0700

    Update

--- lib/actions.ts
+++ lib/actions.ts
@@ -1,13 +1,29 @@
 import { GraphQLClient } from "graphql-request";
 
-const isProduction = process.env.NODE_ENV === 'production'
+const isProduction = process.env.NODE_ENV === "production";
 
-const client = new GraphQLClient('apiUrl')
+const apiUrl = isProduction
+  ? process.env.NEXT_PUBLIC_GRAFBASE_API_URL || ""
+  : "http://127.0.0.1:4000/graphql";
 
-const makeGraphQLRequest = async (query: string, variable = {}) => {
-    try {
+const apiKey = isProduction
+  ? process.env.NEXT_PUBLIC_GRAFBASE_API_KEY || ""
+  : "letmein";
 
-    } catch (error) {
-        throw error;
-    }
+const serverUrl = isProduction
+  ? process.env.NEXT_PUBLIC_SERVER_URL
+  : "http://localhost:3000";
+
+const client = new GraphQLClient(apiUrl);
+
+const makeGraphQLRequest = async (query: string, variables = {}) => {
+  try {
+    return await client.request(query,variables)
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const getUser = (email:string) => {
+    
 }
